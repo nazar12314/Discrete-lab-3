23ee61137e2d882f2eed15412abaf05c5e678fe0
romberol <93192972+romberol@users.noreply.github.com>
2022-05-01 13:43:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 def generate_primes(min, max):
     primes = []
     for i in range(min, max + 1):
-        for j in range(1, int(i ** 0.5) + 1):
+        for j in range(1, int(i**0.5)+1):
             if i % j == 0 and j != 1 and j != i:
                 break
         else:
@@ -19,22 +19,19 @@ def are_relative_primes(first, second):
 
 
 def generate_public_and_private_keys():
-    prime_numbers = generate_primes(1000, 2000)
+    prime_numbers = generate_primes(100, 1000)
     p, q = random.choices(prime_numbers, k=2)
     maximum = (p - 1) * (q - 1)
-
     e = None
     d = None
 
-    for num in range(3, maximum, 2):
+    for num in range(5, maximum, 2):
         if are_relative_primes(num, maximum):
             e = num
-
-        for num_2 in range(3, maximum, 2):
-            if num_2 * e % maximum == 1:
-                d = num_2
-                return (p * q, e), (p * q, d)
-
-a, b = generate_public_and_private_keys()
-print(a)
-print(b)
+            
+            for num_2 in range(3, maximum, 2):
+                if (num_2 * e) % maximum == 1:
+                    d = num_2
+                    return (p * q, e), (p * q, d)
+                
+    raise Exception("Key not generated!")
