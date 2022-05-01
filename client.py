7adf788eb85d37cae5875bbf9794469523a719ab
romberol <93192972+romberol@users.noreply.github.com>
2022-05-01 17:41:18 +0300

    rsa added

--- client.py
+++ client.py
@@ -39,23 +39,14 @@ class Client:
     def read_handler(self):
         while True:
             message = self.s.recv(1024).decode()
-
-            # decrypt message with the secrete key
-
-            # ...
-
-
-            print(message)
+            decoded = rsa.decode(self.private, message)
+            print(decoded)
 
     def write_handler(self):
         while True:
             message = input()
-
-            # encrypt message with the secrete key
-
-            # ...
-
-            self.s.send(message.encode())
+            encoded = rsa.encode(self.public, message)
+            self.s.send(encoded.encode())
 
 if __name__ == "__main__":
     cl = Client("127.0.0.1", 9001, "b_g")
